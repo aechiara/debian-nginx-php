a9809d60d9bbc6aeb93842074a964b4e5e535bc7
Alex Eduardo Chiaranda <aechiara@gmail.com>
2015-01-27 16:55:48 -0200

    added client_max_body_size

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ echo "cgi.fix_pathinfo=0" >> /etc/php5/fpm/php.ini
 cp nginx-default /etc/nginx/sites-available/default
 ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 chown -R www-data:www-data /var/www/website
+sed -ie "/sendfile on;/a\\
+client_max_body_size 10M;" /etc/nginx/nginx.conf
